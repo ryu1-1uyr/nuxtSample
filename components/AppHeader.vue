3df92ec64e1ccd7329b83c38e232e9e4e8d23d99
s17001 <s17001@std.it-college.ac.jp>
2019-04-04 11:37:59 +0900

    飛ばす先のリンクの修正

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -3,7 +3,7 @@
     <nav class="navbar">
       <div class="container">
         <div class="navbar-brand">
-          <nuxt-link to="/breeds" class="navbar-item">sample dog</nuxt-link>
+          <nuxt-link to="/" class="navbar-item">sample dog</nuxt-link>
           <span class="navbar-burger burger" data-target="navbarMenu">
             <span></span>
             <span></span>
