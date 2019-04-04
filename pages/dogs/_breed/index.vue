479a50da1c47a18a67a2cd2e8462942ec38ac1f2
s17001 <s17001@std.it-college.ac.jp>
2019-04-04 11:38:28 +0900

    いいねとNEWの仮実装

--- pages/dogs/_breed/index.vue
+++ pages/dogs/_breed/index.vue
@@ -1,6 +1,16 @@
 <template>
   <section class="container">
     <div class="columns is-multiline">
+
+      <div v-for="(item, i) in dog_list" v-bind:key="i" class="column is-1">
+        <img :src="item.url">
+
+        <!-- いいねとNEWの仮実装をしてみたやつ(サーバーにデータは送信されない) -->
+        <span v-if="i < 3" class="tag is-danger">NEW</span>
+        <a class="button is-warning  is-small" v-on:click="item.like += 1">
+          <span>いいね！{{item.like}}件</span>
+        </a>
+      </div>
     </div>
   </section>
 </template>
